c338e17a37fc09f3f8133d10c71e1403fb5968c9
Alptekin I <alptekin@192.168.1.7>
2022-10-10 23:54:33 +0300

    app() changed to class component. Some little exercise on setState done.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {
+        "main-name": "world",
+        says: "rules",
+      },
+      when: "always",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            hello {this.state.name["main-name"]} {this.state.name.says}{" "}
+            {this.state.when}
+          </p>
+          <button
+            onClick={() => {
+              console.log("button clicked");
+              this.setState({
+                name: { "main-name": "alptekin", says: "rules" },
+              });
+            }}
+          >
+            Change name{" "}
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
